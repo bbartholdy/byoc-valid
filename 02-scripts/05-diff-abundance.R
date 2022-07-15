ce4fcb987db95f3f54d5247b7ec73407ce457739
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2022-07-15 16:54:37 +0200

    rearrange diff abund code

---
+++ 02-scripts/05-diff-abundance.R
@@ -0,0 +1,202 @@
+# Compare species abundance for biofilm samples and modern plaque/calculus
+  # compare aerobic and anaerobic
+  # genus-level comparison
+library(tidyverse)
+library(phyloseq)
+library(ANCOMBC)
+taxa_table <- readr::read_tsv("05-results/post-decontam_taxatable.tsv")
+analysis_metadata <- readr::read_tsv("01-documentation/analysis-metadata.tsv")
+experiment_metadata <- readr::read_tsv("01-documentation/experiment-metadata.tsv")
+
+# function to calculate bias-corrected log-observed abundances
+# from vignette("ANCOMBC")
+
+#' function to calculate bias-corrected log-observed abundances
+#' from `vignette("ANCOMBC")`
+#' 
+#' @param x `ancombc` object.
+bias_correct <- function(x, otu_table) {
+  require(microbiome)
+  samp_frac <- x$samp_frac
+  samp_frac[is.na(samp_frac)] <- 0 # replace NAs with 0
+  log_otu <- log(microbiome::abundances(otu_table) + 1)
+  log_otu_adj <- exp(t(t(log_otu) - samp_frac)) %>% # bias corrected abund
+    as_tibble(rownames = "species")
+  return(log_otu_adj)
+}
+
+
+# Data prep ---------------------------------------------------------------
+
+taxa_table_long <- taxa_table %>%
+  pivot_longer(cols = where(is.numeric), names_to = "sample", values_to = "count")
+
+# convert otu tables to phyloseq object
+byoc_matrix <- taxa_table_long %>%
+  filter(str_detect(sample, "SYN")) %>%
+  pivot_wider(names_from = "sample", values_from = "count") %>%
+  column_to_rownames(var = "#OTU ID") %>%
+  otu_table(taxa_are_rows = T)
+
+byoc_meta <- experiment_metadata %>%
+  filter(`#SampleID` %in% analysis_metadata$`#SampleID`) %>%
+  column_to_rownames(var = "#SampleID") %>% 
+  sample_data()
+
+byoc_phyloseq <- phyloseq(byoc_matrix, byoc_meta)
+
+plaque_matrix <- taxa_table_long %>%
+  filter(
+    sample %in% filter(
+      analysis_metadata, str_detect(Env, "calculus|plaque") # comparative samples and model calculus
+      )$`#SampleID`
+    ) %>%
+  pivot_wider(names_from = "sample", values_from = "count") %>%
+  column_to_rownames(var = "#OTU ID") %>%
+  otu_table(taxa_are_rows = T)
+
+plaque_meta <- analysis_metadata %>%
+  filter(str_detect(Env, "calculus|plaque")) %>%
+  column_to_rownames(var = "#SampleID") %>%
+  sample_data()
+
+calculus_phyloseq <- phyloseq(plaque_matrix, plaque_meta)
+
+
+# Differential abundance --------------------------------------------------
+
+# across experiment
+exp_da <- ANCOMBC::ancombc(
+  experiment_phyloseq, 
+  formula = "Env",
+  group = "Env",
+  global = F,
+  p_adj_method = "fdr"
+  )
+
+# plaque and calculus
+plaque_da <- ANCOMBC::ancombc(
+  plaque_phyloseq, 
+  formula = "Env",
+  group = "Env",
+  global = F,
+  p_adj_method = "fdr"
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# function to get pallette for top species/genus
+
+function(x, pallette, n) {
+  col_pallette <- palette(n = n)
+  names(col_pallette) <- levels(x)
+  col_scale <- scale_fill_manual(name = "", values = col_pallette)
+}
+#my_pallette <- c(viridisLite::viridis(n = length(top_species_names)), "grey") 
+#names(my_pallette) <- levels(top_abundance$top_species)
+#my_scale <- scale_fill_manual(name = "top_species", values = my_pallette)
+
+
+
+
+# Diff abundance ----------------------------------------------------------
+
+exp_day <- ANCOMBC::ancombc(
+  experiment_phyloseq, 
+  formula = "Env",
+  group = "Env",
+  global = T,
+  p_adj_method = "fdr")
+
+ANCOMBC::ancom(experiment_phyloseq, main_var = "Env")
+
+# from vignette("ANCOMBC")
+samp_frac <- exp_day$samp_frac
+# Replace NA with 0
+samp_frac[is.na(samp_frac)] = 0 
+# add 1 to counts for log-transformation
+log_exp_otu <- log(microbiome::abundances(experiment_otu) + 1)
+
+# Adjust the log observed abundances
+log_exp_otu_adj = t(t(log_exp_otu))
+# Bias-corrected log observed abundances
+  # Show the first 6 samples
+round(log_exp_otu_adj[, 1:6], 2) %>% 
+  as_tibble(rownames = "species") %>%
+  .$SYN001.A0101
+
+class(otu_table(taxatable_matrix, taxa_are_rows = T))
+
+
+
+#da_env_taxa <- ANCOMBC::ancombc(otu_phyloseq, formula = "Env")
+
+# taxatable_abundance_long <- taxatable %>%
+#   pivot_longer(cols = where(is.numeric), names_to = "sample", values_to = "count") %>% 
+#   rename(species = `#OTU ID`) %>% 
+#   group_by(sample) %>%
+#   mutate(rel_abund = count / sum(count))
+# 
+# species_abundance_long <- taxatable_abundance_long %>%
+#   #bind_rows(taxatable_abundance) %>%
+#   mutate( # fix species names
+#     species = str_remove(species, "\\["), # remove square brackets from names
+#     species = str_remove(species, "\\]") # probably not the most elegant solution
+#     ) 
+#   
+# 
+# top_species_names <- species_abundance_long %>% 
+#   group_by(sample) %>% 
+#   arrange(desc(rel_abund)) %>% 
+#   slice_head(n = 4) %>% # take the top 4 species from each sample
+#   .$species %>% 
+#   unique()
+# 
+# genus_abundance_long <- species_abundance_long %>%
+#   mutate(genus = str_extract(species, "^([\\w\\-]+)")) %>% 
+#   dplyr::select(!c(species,rel_abund)) %>% 
+#   group_by(sample, genus) %>%
+#   summarise(count = sum(count, na.rm = T)) %>% 
+#   mutate(rel_abund = count / sum(count))
+# 
+# top_genus_names <- genus_abundance_long %>% 
+#   group_by(sample) %>% 
+#   arrange(desc(rel_abund)) %>% 
+#   slice_head(n = 4) %>% 
+#   .$genus %>% 
+#   unique()
+
+# Export data -------------------------------------------------------------
+
+#write_tsv(species_abundance_long, "05-results/species-abundance_long.tsv")
+#write_tsv(genus_abundance_long, "05-results/genus-abundance_long.tsv")
+
