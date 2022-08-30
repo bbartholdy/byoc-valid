470b02566568eba3b07af62bfd5f6a4a92ef0e3c
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2022-08-30 21:40:23 +0200

    work on manuscript

--- 07-publication/_bib.R
+++ 07-publication/_bib.R
@@ -3,16 +3,17 @@ library(rbbt)
 
 file_names <- list.files(here("07-publication"), pattern = ".qmd")
 
+
+try(
 if(file.exists(here("07-publication/references.bib"))) {
   bbt_update_bib(here("07-publication", file_names), 
                  here("07-publication/references.bib"),
                  overwrite = T)
 } else {
-  refs <- rbbt::bbt_detect_citations(file_names)
   bbt_write_bib(here("07-publication/references.bib"), 
-                keys = refs, 
+                keys = rbbt::bbt_detect_citations(file_names), 
                 ignore = bbtignore, 
                 overwrite = T, 
                 translator = "bibtex")
 }
-
+)
