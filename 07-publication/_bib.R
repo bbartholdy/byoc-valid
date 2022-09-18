1deb8a145ddca73706d825480bfe97bc8e6be957
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2022-09-18 10:55:29 +0200

    clean up output figures

--- 07-publication/_bib.R
+++ 07-publication/_bib.R
@@ -1,19 +1,27 @@
 library(here)
 library(rbbt)
+library(stringr)
 
-file_names <- list.files(here("07-publication"), pattern = ".qmd")
-
+file_names <- list.files(here("07-publication"), pattern = ".qmd", full.names = T)
+file_names <- file_names[str_detect(file_names, "_", negate = T)]
+bbtkeys <- bbt_detect_citations(file_names)
+bbtignore <- bbtkeys[str_detect(bbt_detect_citations(file_names), "^fig-")]
 
 try(
 if(file.exists(here("07-publication/references.bib"))) {
-  bbt_update_bib(here("07-publication", file_names), 
-                 here("07-publication/references.bib"),
-                 overwrite = T)
+  bbt_update_bib(
+    file_names, 
+    here("07-publication/references.bib"),
+    overwrite = T,
+    ignore = bbtignore
+    )
 } else {
-  bbt_write_bib(here("07-publication/references.bib"), 
-                keys = rbbt::bbt_detect_citations(file_names), 
-                ignore = bbtignore, 
-                overwrite = T, 
-                translator = "bibtex")
+  bbt_write_bib(
+    here("07-publication/references.bib"), 
+    keys = bbtkeys, 
+    ignore = bbtignore, 
+    overwrite = T, 
+    translator = "bibtex"
+    )
 }
 )
