4273ae6d32930cf058a7a36afc466f00b4301287
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-01-21 13:34:39 +0100

    add references to supp-mat

--- 07-publication/_bib.R
+++ 07-publication/_bib.R
@@ -3,7 +3,7 @@ library(rbbt)
 library(stringi)
 
 file_names <- list.files(here("07-publication"), pattern = ".qmd", full.names = T)
-file_names <- file_names[stri_detect(file_names, regex = "_", negate = T)]
+#file_names <- file_names[stri_detect(file_names, regex = "_", negate = T)]
 bbtkeys <- bbt_detect_citations(file_names)
 bbtignore <- bbtkeys[stri_detect(bbt_detect_citations(file_names), regex = "^fig-|^tbl-")] # ignore table and figure cross-references
 
